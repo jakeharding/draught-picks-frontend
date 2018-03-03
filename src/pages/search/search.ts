5b0e9afc1b4a2862c3e23bcf22f18dcd5897e4a3
Quinn Adams <qadams@unomaha.edu>
2018-03-03 17:03:20 -0600

    feature/searchForBeer: Message changes correctly now

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -23,19 +23,30 @@ export class SearchPage {
 
   beerResults: Array<Beer>;
   beerSearch: string;
-
+  message: string;
+  private static NULL_RESULT_MESSAGE = "Search for a beer and let us know what you liked about it.";
+  private static ZERO_RESULT_MESSAGE = "We cannot find results on the beers you have entered.";
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public beerProvider: BeerProvider) {
+    this.message = SearchPage.NULL_RESULT_MESSAGE;
   }
 
   search (event:Event) {
     if(this.beerSearch && this.beerSearch.length > 2) {
         this.beerProvider.search(this.beerSearch).then((results: Array<Beer>) => {
             this.beerResults = results;
+            if(this.beerResults.length === 0) {
+                this.message = SearchPage.ZERO_RESULT_MESSAGE;
+            }
         });
     } else {
         this.beerResults = []
+        this.message = SearchPage.NULL_RESULT_MESSAGE;
     }
   }
+  clear(event:Event) {
+    this.message = SearchPage.NULL_RESULT_MESSAGE;
+    this.beerResults = null;
 
+  }
 }
