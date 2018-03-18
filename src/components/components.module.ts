20655c944b501252c56ed39309d6bab322afc05d
Jake Harding <jacobeharding@gmail.com>
2018-03-18 11:13:42 -0500

    Add draught header and logout.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,15 +4,18 @@ import { BeerListComponent } from './beer-list/beer-list';
 import { BeerItemComponent } from './beer-item/beer-item';
 import {IonicModule} from "ionic-angular";
 import { RatingComponent } from './rating/rating';
+import { DraughtHeaderComponent } from './draught-header/draught-header';
 @NgModule({
 	declarations: [BeerListComponent,
     BeerItemComponent,
-    RatingComponent],
+    RatingComponent,
+    DraughtHeaderComponent],
 	imports: [
     IonicModule, CommonModule,
 	],
 	exports: [BeerListComponent,
     BeerItemComponent,
-    RatingComponent]
+    RatingComponent,
+    DraughtHeaderComponent]
 })
 export class ComponentsModule {}
