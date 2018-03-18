d9f9b0680ecbd73eb212cdf79586cd8d9308c0b5
Jake Harding <jacobeharding@gmail.com>
2018-03-18 11:31:50 -0500

    Clicking title in header will go to home page.

--- src/components/draught-header/draught-header.ts
+++ src/components/draught-header/draught-header.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthProvider} from "../../providers/auth/auth";
+import {TabsPage} from "../../pages/tabs/tabs";
+import {NavController} from "ionic-angular";
 
 /**
  * Generated class for the DraughtHeaderComponent component.
@@ -15,13 +17,20 @@ export class DraughtHeaderComponent {
 
   isLoggedIn: boolean;
 
-  constructor(public authProvider: AuthProvider) {
+  constructor(public authProvider: AuthProvider, public navCtrl: NavController) {
     this.isLoggedIn = this.authProvider.isLoggedIn();
   }
 
   logout () {
     this.authProvider.clearToken();
     location.hash = '/sign-in';
+  }
+
+  home () {
+    if (this.authProvider.isLoggedIn()) {
+      location.hash = '/';
+      this.navCtrl.parent.select(1);
+    }
   }
 
 }
