500cee7a7f35b4c43886f0f5558c04be1ed7f3a1
Jake Harding <jacobeharding@gmail.com>
2018-11-15 20:58:54 -0600

    Add beer list spec.

---
+++ src/components/beer-list/beer-list.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * beer-list.spec.ts
+ *
+ * Created by jake
+ * Created on 11/15/18
+ *
+ * Test the beer list component.
+ */
+import { BeerListComponent } from "./beer-list";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { BeerItemComponent } from "../beer-item/beer-item";
+import { ConfirmEmailPage } from "../../pages/confirm-email/confirm-email";
+import { NavPush, ToastController } from "ionic-angular";
+import { RatingProvider } from "../../providers/rating/rating";
+import { RatingComponent } from "../rating/rating";
+
+describe('BeerListComponent', () => {
+  let component: BeerListComponent;
+  let fixture: ComponentFixture<BeerListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [BeerListComponent, BeerItemComponent, NavPush, RatingComponent],
+      providers: []
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BeerListComponent);
+    component = fixture.debugElement.componentInstance;
+  });
+
+  it('should initialize the BeerListComponent', () => {
+    expect(component).toBeDefined();
+    expect(fixture).toMatchSnapshot();
+  });
+});
+
