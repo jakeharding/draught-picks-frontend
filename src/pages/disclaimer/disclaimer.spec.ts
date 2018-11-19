192e18b38bf6368b4687c1ed40ceecc949f95fb4
Jake Harding <jacobeharding@gmail.com>
2018-11-18 20:57:18 -0600

    Lint fixing.

--- src/pages/disclaimer/disclaimer.spec.ts
+++ src/pages/disclaimer/disclaimer.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DisclaimerPage } from './disclaimer';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 /**
  * disclaimer.spec.ts
