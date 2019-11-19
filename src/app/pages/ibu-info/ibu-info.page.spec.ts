3d675aa3c8d3f9fae4ddf228e1faf5afe527da6a
Jake Harding <jacobeharding@gmail.com>
2019-11-18 20:29:25 -0600

    Ibu info page spec.

--- src/app/pages/ibu-info/ibu-info.page.spec.ts
+++ src/app/pages/ibu-info/ibu-info.page.spec.ts
@@ -1,27 +1,35 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IbuInfoPage } from './ibu-info.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/**
+ * ibupage.spec.ts
+ *
+ * Created by jake
+ * Created on 11/18/18
+ *
+ * Test IbuInfoPage.
+ */
 
 describe('IbuInfoPage', () => {
-  let component: IbuInfoPage;
   let fixture: ComponentFixture<IbuInfoPage>;
+  let instance: IbuInfoPage;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IbuInfoPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+      declarations: [IbuInfoPage],
+      providers: []
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IbuInfoPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    instance = fixture.debugElement.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initialize the IbuInfoPage', () => {
+    expect(instance).toBeTruthy();
+    expect(fixture).toMatchSnapshot();
   });
 });
